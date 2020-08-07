1b1086a188ba9b8486c3ee09fe5743a4290b4836
Kinnari Shah <kinnari1022@yahoo.com>
2020-08-07 02:55:43 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -9,11 +9,11 @@ attr_reader :name, :backed_projects
    @backed_projects = []
  end
   
-  def back_project(project)
-    @backed_projects << project
+  def back_project(project, backer)
+    @backed_projects << project, backer
     
   end
   
-  def back_project
-  
-end
+#   def back_project
+#   ba
+# end
